54090eba96075a3d020776777ea06cd24fff1b6c
Chołda Marcin <marcin.cholda@microsoft.wsei.edu.pl>
2021-10-16 13:45:10 +0200

    Added Book MVC

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LabBack;
+using LabBack.Models;
+
+namespace LabBack.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Random()
+        {
+            var firstBook = new Book() { Name = "English Dictionary" };
+            return View(firstBook);
+        }
+
+    }
+}
